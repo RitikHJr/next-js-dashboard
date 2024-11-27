70f147ee4141502b83e28e1099d5af5ad2a02704
Ritik Bhatia <ritikhjr@gmail.com>
2024-11-28 01:55:43 +0530

    Chapter 14

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,7 +1,6 @@
 import { lusitana } from "@/app/ui/fonts";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
-import { Card } from "@/app/ui/dashboard/cards";
 import { Suspense } from "react";
 import {
   LatestInvoicesSkeleton,
